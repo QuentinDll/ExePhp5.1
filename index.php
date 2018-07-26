2db5807cd044cc0be150857c95e8ea4b4bce984a
QuentinDll <quentin.delplace13@gmail.com>
2018-07-26 16:21:28 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 5</title>
+</head>
+<body>
+  <p>
+    <?php
+    $int = null;
+    echo $int;
+    echo $int += '1';
+    ?>
+  </p>
+</body>
+</html>
